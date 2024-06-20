544ccca8f8594f95609c2dea14f3948694a29a21
Ёж Соник <g.mens.hl@gmail.com>
2024-06-20 21:08:55 +0300

    Добавьте файлы проекта.

---
+++ OpenTetrisSDL/OpenTetrisSDL.cpp
@@ -0,0 +1,12 @@
+﻿// OpenTetrisSDL.cpp: определяет точку входа для приложения.
+//
+
+#include "OpenTetrisSDL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
